947a2fed0e4bc55dbee39a258c4962f745e4376e
Juan Ramón Mateo <probably.unwise@protonmail.ch>
2020-07-24 10:57:59 -0500

    changed to f strings

--- posts_per_day.py
+++ posts_per_day.py
@@ -1,9 +1,19 @@
+# Take the date you registered on a forum and counts how many
+# posts per day you would have to make to cath up
 import datetime
 
 register_date = datetime.date(2011, 12, 3)
 todays_date = datetime.date.today()
 days_old = (todays_date-register_date).days
-posts_per_day = round(days_old/365)
-print("You need to make " + str(posts_per_day) + " posts every day")
-print("to catch up in one year, or " + str(posts_per_day*2))
-print("posts per day to do it six months. Happy posting!")
+
+current_posts = 260
+needed_posts = days_old-current_posts
+posts_per_day = round(needed_posts/365)
+
+out_msg = (
+    f"You need to make {posts_per_day} posts every day to\n"
+    f"catch up in one year, or {posts_per_day*2} posts\n"
+    f"per day to do it six months."
+)
+
+print(out_msg)
